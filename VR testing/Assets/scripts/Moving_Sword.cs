ac817bc64339b9699cf8d873219dcb5b0b3e91ab
MarinCombot <marin.combot@hotmail.fr>
2017-09-29 12:22:57 -0400

    testing moving vr hand

---
+++ VR testing/Assets/scripts/Moving_Sword.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Sword :SteamVR_TrackedController {
+
+
+		public SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) controllerIndex); } }
+		public Vector3 velocity { get { return controller.velocity; } }
+		public Vector3 angularVelocity { get { return controller.angularVelocity; } }
+
+		void update(){
+			Debug.Log ("" + controller.velocity);
+			Debug.Log ("" + controller.angularVelocity);
+
+		}
+
+	}
