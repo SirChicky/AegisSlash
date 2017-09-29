43fba91da1dc3a9ae24cec6282cd632473379726
Xuan Yan Huang <x.huang@student.isartdigital.com>
2017-09-29 11:41:26 -0400

    Added the haptic pulse trigger script

---
+++ VR testing/Assets/scripts/TriggerHapticPulse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHapticPulse : MonoBehaviour {
+
+	private ushort vibratingTime;
+	SteamVR_TrackedObject trackedObj;
+
+	void Awake ()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.gameObject.CompareTag ("buncing")) {
+			Debug.Log ("Wack!");
+			vibratingTime = 180;
+			SteamVR_Controller.Input ((int)trackedObj.index).TriggerHapticPulse (vibratingTime);
+		}
+	}
+}
