037197c8e1a931ab0547778d4b662207891de1e9
ISART\r.geierfernandez <m.combot@student.isartdigital.com>
2017-10-04 17:00:46 -0400

    the sword velocity is tracked

--- VR testing/Assets/scripts/Moving_Sword.cs
+++ VR testing/Assets/scripts/Moving_Sword.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moving_Sword :SteamVR_TrackedController {
+public class Moving_Sword :SteamVR_TrackedController 
+{
+	private SteamVR_TrackedObject trackedObj;
+	public SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) controllerIndex); } }
+	public Vector3 velocity { get { return controller.velocity; } }
+	public Vector3 angularVelocity { get { return controller.angularVelocity; } }
+	//private SteamVR_Controller.Device controller;
 
 
-		public SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) controllerIndex); } }
-		public Vector3 velocity { get { return controller.velocity; } }
-		public Vector3 angularVelocity { get { return controller.angularVelocity; } }
-
-		void update(){
-			Debug.Log ("" + controller.velocity);
-			Debug.Log ("" + controller.angularVelocity);
-
-		}
-
+	void Start()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
 	}
+
+	void FixedUpdate() 
+	{
+		//controller = SteamVR_Controller.Input((int)trackedObj.index);
+	}
+
+	void Update()
+	{
+		Debug.Log ("" + controller.velocity);
+		Debug.Log ("" + controller.angularVelocity);
+	}
+}
