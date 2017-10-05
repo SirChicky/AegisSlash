f2065ca342e3a7bbe880e38179f87b8cda48769a
MarinCombot <marin.combot@hotmail.fr>
2017-10-05 13:59:57 -0400

    modification du script pour que les limites float soient accessibles

--- VR testing/Assets/scripts/Moving_Sword.cs
+++ VR testing/Assets/scripts/Moving_Sword.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Moving_Sword :SteamVR_TrackedController 
 {
+	public float limitx;
+	public float limity;
+	public float limitz;
+
+	public float limitangx;
+	public float limitangy;
+	public float limitangz;
+
 	private SteamVR_TrackedObject trackedObj;
 	public SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int) controllerIndex); } }
 	public Vector3 velocity { get { return controller.velocity; } }
 	public Vector3 angularVelocity { get { return controller.angularVelocity; } }
+
+
 	//private SteamVR_Controller.Device controller;
 
 
@@ -25,5 +35,30 @@ public class Moving_Sword :SteamVR_TrackedController
 	{
 		Debug.Log ("" + controller.velocity);
 		Debug.Log ("" + controller.angularVelocity);
-	}
+
+		if (controller.velocity.x>limitx)
+			{
+				Debug.Log ("oh my goodness");
+		}
+		if (controller.velocity.y>limity)
+		{
+				Debug.Log ("oh my god");
+		}	
+		if (controller.velocity.z>limitz)
+		{
+				Debug.Log ("oh no");
+		}
+		if (controller.velocity.x>limitangx)
+		{
+			Debug.Log ("Star platinum");
+		}
+		if (controller.velocity.y>limitangy)
+		{
+			Debug.Log ("Silver chariot");
+		}	
+		if (controller.velocity.z>limitangz)
+		{
+			Debug.Log ("The world(O)");
+		}
+}
 }
