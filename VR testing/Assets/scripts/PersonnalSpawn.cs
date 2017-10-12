468c97a030c047ccb43fe28c4a25cd50d6779038
MarinCombot <marin.combot@hotmail.fr>
2017-10-12 14:26:38 -0400

    Personnal spawn prefab and script

---
+++ VR testing/Assets/scripts/PersonnalSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonnalSpawn : MonoBehaviour {
+	public  GameObject Enemy;
+	public float Timer;
+	public string objectToFind;
+	void Awake() {
+		Timer = Time.time + 3;
+	}
+
+	void Update() {
+		if (Timer < Time.time && !GameObject.Find(objectToFind)) { //This checks wether real time has caught up to the timer
+			Instantiate(Enemy, transform.position, transform.rotation);//This spawns the emeny
+			//Timer = Time.time + 3; //This sets the timer 3 seconds into the future
+		}
+	}
+
+}
