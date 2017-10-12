7b2e26e559933f37c1f36a491795117e6733c629
MarinCombot <marin.combot@hotmail.fr>
2017-10-12 16:17:44 -0400

    modification du code perconnal spawn point

--- VR testing/Assets/scripts/PersonnalSpawn.cs
+++ VR testing/Assets/scripts/PersonnalSpawn.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PersonnalSpawn : MonoBehaviour {
-	public  GameObject enemy;
-	private GameObject myenemy;
+	
+		     // Reference to the player's heatlh.
+		public GameObject enemy;
+		private GameObject myenemy;// The enemy prefab to be spawned.
+		//public float spawnTime = 3f;            // How long between each spawn.
+		public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	    public bool existence;
 
-	void Update() {
-		if (myenemy==null) { //This checks wether real time has caught up to the timer
-			myenemy=Instantiate(enemy, transform.position, transform.rotation);//This spawns the emeny
+		void Update ()
+		{
+			// If the player has no health left...
+		if(existence==true && myenemy==null)
+			{
 
+			// Find a random index between zero and one less than the number of spawn points.
+	      	int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+			// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+			myenemy = Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);	}
+		}
 	}
 
-}
-}
