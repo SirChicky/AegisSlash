f0b87fae6c48e8819b96bb381071e0983c36e3ed
MarinCombot <marin.combot@hotmail.fr>
2017-10-24 10:32:35 -0400

    SlashBack script for activate box trigger behind the Player

---
+++ VR testing/Assets/scripts/SlashBack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashBack : MonoBehaviour {
+
+
+	void OnTriggerEnter (Collider Other){
+		if (Other.gameObject.tag == ("Sword")) {
+			this.gameObject.tag = ("Sword");
+
+		
+		} 
+			if (Other.gameObject.tag == ("IdleSword"))
+			{
+
+				this.gameObject.tag=("idleSword");
+			}
+
+			else {
+			this.gameObject.tag = ("Untagged");
+		}
+		
+	}
+}
+
