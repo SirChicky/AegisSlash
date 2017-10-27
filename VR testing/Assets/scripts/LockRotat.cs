f9fd665df5d2f80d5a9d92b45f67b7ae53e67161
SNSV-LAPTOP\ChickyLaptop <romariogeier@gmail.com>
2017-10-27 12:10:09 -0400

    LockRotationScript

--- VR testing/Assets/scripts/LockRotat.cs
+++ VR testing/Assets/scripts/LockRotat.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LockRotat : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	float lockPos = 0;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.y, lockPos, lockPos); //Used to lock and Free some angles for rotation purposes
 	}
 }
