4888f8ba4033cfcd697df127958eabea95637d8b
ISART\r.geierfernandez <m.combot@student.isartdigital.com>
2017-11-08 14:57:52 -0500

    Screenshots

--- VR testing/Assets/scripts/Work in progress/Super.cs
+++ VR testing/Assets/scripts/Work in progress/Super.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Super : MonoBehaviour {
 	public float speed;
-
+	public float selfDestruct;
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position += transform.forward * (Time.deltaTime * speed);
+		Destroy (this.gameObject,selfDestruct);
 	}
 }
