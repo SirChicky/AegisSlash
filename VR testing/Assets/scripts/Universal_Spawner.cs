919fda5cc3836f368089291270f0dd25bf76e316
MarinCombot <marin.combot@hotmail.fr>
2017-11-21 10:07:47 -0500

    MAj 2 du spawner universel

--- VR testing/Assets/scripts/Universal_Spawner.cs
+++ VR testing/Assets/scripts/Universal_Spawner.cs
@@ -8,14 +8,18 @@ public class Universal_Spawner : MonoBehaviour {
 		public List<GameObject> enemies = new List<GameObject> ();
 		public List<Transform> spawnposition = new List<Transform> ();
 		public List<bool> grouped = new List<bool> ();
-	    public List<bool> inGroup = new List<bool> ();
+		public List<bool> inGroup = new List<bool> ();
 		private int spawnsOrigin;
 		private int spawnsNumber;
+		private bool stop;
+		private bool stopTheMachine;
 		private int spawnsNumber2;
 		public int spawnsStart;
 		void Awake(){
 			spawnsOrigin = 0;
 			spawnsNumber = spawnsOrigin;
+			stopTheMachine = false;
+			stop = false;
 		}
 
 		void Start () {
@@ -24,55 +28,76 @@ public class Universal_Spawner : MonoBehaviour {
 				spawnsStart -= 1;
 				spawnsNumber = spawnsStart;
 			}
-		if (spawnsStart == 0) {
-			spawnsNumber = 1;
-		}
+			if (spawnsStart == 0) {
+				spawnsNumber = 1;
+			}
 		}
 
 		void Update () {
-			//FailSafe ();
+			FailSafe ();
+			FailSafe2 ();
 			spawnsNumber2.Equals (GameObject.FindGameObjectWithTag ("Enemy"));
-		if (inGroup[0] = true) {
-			spawnsNumber = 0;
-			if ((spawnsNumber2 ==spawnsNumber) || grouped [0] == true) {
-				if (spawnsStart <= 0 && spawnsNumber <= 0) {
-					timer [0] -= Time.deltaTime;
-					if (timer [0] <= 0.0f) {
-						Spawn ();
-						spawnsNumber = spawnsNumber2;
+			if (stopTheMachine == false) {
+				if (inGroup [0] = true) {
+					spawnsNumber = 0;
+					if ((spawnsNumber2 == spawnsNumber) || grouped [0] == true) {
+						if (spawnsStart <= 0 && spawnsNumber <= 0) {
+							timer [0] -= Time.deltaTime;
+							if (timer [0] <= 0.0f) {
+								Spawn ();
+								spawnsNumber = spawnsNumber2;
+							}
+						}
+					}
+				}
+			}
+			if (stopTheMachine == false) {
+				if (inGroup [0] = false ) {
+					if ((spawnsNumber != spawnsNumber2) || grouped [0] == true) {
+						if (spawnsStart <= 0 && spawnsNumber <= 0) {
+							timer [0] -= Time.deltaTime;
+							if (timer [0] <= 0.0f) {
+								Spawn ();
+								spawnsNumber = spawnsNumber2;
+							}
+						}
 					}
 				}
 			}
 		}
-		if (inGroup[0]=false){
-			if ((spawnsNumber != spawnsNumber2)||grouped[0]==true) {
-				if (spawnsStart <= 0 && spawnsNumber <= 0) {
-					timer [0] -= Time.deltaTime;
-					if (timer [0] <= 0.0f) {
-						Spawn ();
-						spawnsNumber = spawnsNumber2;
-					}
-				  }
-				}
-			}
-		}
-
 		void Spawn(){
 			Instantiate (enemies[0],spawnposition[0]);
 			enemies.Remove(enemies[0]) ;
 			spawnposition.Remove(spawnposition[0]);
 			timer.Remove (timer [0]);
 			grouped.Remove(grouped[0]);
-		    inGroup.Remove (inGroup [0]);
-		    
+			inGroup.Remove (inGroup [0]);
+
 		}
-		//void FailSafe(){
-		//if (enemies [0].Equals( enemies.FindLastIndex(enemies))){
-		///	enemies.Add (enemies [0]);
-		///timer.Add (timer [0]);
-		//spawnposition.Add (spawnposition [0]);
-		//}
+		void FailSafe(){
+			if (stopTheMachine == false) {
+				if (enemies.Count == 1 || timer.Count == 1 || grouped.Count == 1 || spawnposition.Count == 1 || grouped.Count == 1 || inGroup.Count == 1) {
+					Debug.Log ("the end");
+					if (stop == true) {
+						stopTheMachine = true;
+					}
 
+				}
+			}
+		}
+		void FailSafe2(){
+			if (stopTheMachine == false&&stop==false) {
+				if (enemies.Count ==2 || timer.Count == 2 || grouped.Count == 2 || spawnposition.Count == 2 || grouped.Count == 2 || inGroup.Count == 2) {
+					Debug.Log ("the end");
+					enemies.Add (enemies [1]);
+					spawnposition.Add(spawnposition[1]);
+					timer.Add(timer [1]);
+					grouped.Add(grouped[1]);
+					inGroup.Add (inGroup [1]);
+					stop = true;
 
-		//}
+				}
+			}
+			
+		}
 	}
